69e33ce7b6d7493a7998f5613010bbc73aaccec8
Manoj kumar <129170181+Manoj2204@users.noreply.github.com>
2023-11-28 16:03:45 +0530

    Update test1.java

--- src/test/java/Test/test1.java
+++ src/test/java/Test/test1.java
@@ -3,7 +3,7 @@ package Test;
 public class test1 
 {
 	public static void main(String[] args) {
-		System.out.println("Divya");
+		
 		System.out.println("Salma");
 		System.out.println("Manoj");
 		
